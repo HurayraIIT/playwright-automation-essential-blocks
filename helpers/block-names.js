19f61230b433acf2a62caf82cc5e809b1da4c8b3
Abu Hurayra <hurayraiit@gmail.com>
2024-06-27 02:38:51 +0600

    added test for button block

---
+++ helpers/block-names.js
@@ -0,0 +1,71 @@
+"use strict";
+
+// FREE BLOCKS: https://github.com/WPDevelopers/essential-blocks/blob/master/blocks/accordion/block.json
+// PRO BLOCKS: https://github.com/WPDevelopers/essential-blocks-pro/blob/main/blocks/advanced-search/block.json
+
+const EB_Free_Blocks = Object.freeze({
+  ACCORDION_ITEM: "essential-blocks/accordion-item",
+  ACCORDION: "essential-blocks/accordion",
+  ADVANCED_HEADING: "essential-blocks/advanced-heading",
+  ADVANCED_IMAGE: "essential-blocks/advanced-image",
+  ADVANCED_NAVIGATION: "essential-blocks/advanced-navigation",
+  ADVANCED_TABS: "essential-blocks/advanced-tabs",
+  ADVANCED_VIDEO: "essential-blocks/advanced-video",
+  BUTTON: "essential-blocks/button",
+  CALL_TO_ACTION: "essential-blocks/call-to-action",
+  COLUMN: "essential-blocks/column",
+  COUNTDOWN: "essential-blocks/countdown",
+  DUAL_BUTTON: "essential-blocks/dual-button",
+  FEATURE_LIST: "essential-blocks/feature-list",
+  FLIPBOX: "essential-blocks/flipbox",
+  FLUENT_FORMS: "essential-blocks/fluent-forms",
+  FORM_CHECKBOX_FIELD: "essential-blocks/form-checkbox-field",
+  FORM_EMAIL_FIELD: "essential-blocks/form-email-field",
+  FORM_NUMBER_FIELD: "essential-blocks/form-number-field",
+  FORM_RADIO_FIELD: "essential-blocks/form-radio-field",
+  FORM_SELECT_FIELD: "essential-blocks/form-select-field",
+  FORM_TEXT_FIELD: "essential-blocks/form-text-field",
+  FORM_TEXTAREA_FIELD: "essential-blocks/form-textarea-field",
+  FORM: "essential-blocks/form",
+  GOOGLE_MAP: "essential-blocks/google-map",
+  ICON: "essential-blocks/icon",
+  IMAGE_COMPARISON: "essential-blocks/image-comparison",
+  IMAGE_GALLERY: "essential-blocks/image-gallery",
+  INFOBOX: "essential-blocks/infobox",
+  INSTAGRAM_FEED: "essential-blocks/instagram-feed",
+  INTERACTIVE_PROMO: "essential-blocks/interactive-promo",
+  NFT_GALLERY: "essential-blocks/nft-gallery",
+  NOTICE: "essential-blocks/notice",
+  NUMBER_COUNTER: "essential-blocks/number-counter",
+  OPENVERSE: "essential-blocks/openverse",
+  PARALLAX_SLIDER: "essential-blocks/parallax-slider",
+  POPUP: "essential-blocks/popup",
+  POST_CAROUSEL: "essential-blocks/post-carousel",
+  POST_GRID: "essential-blocks/post-grid",
+  PRICE: "essential-blocks/price",
+  PRICING_TABLE: "essential-blocks/pricing-table",
+  PROGRESS_BAR: "essential-blocks/progress-bar",
+  ROW: "essential-blocks/row",
+  SHAPE_DIVIDER: "essential-blocks/shape-divider",
+  SLIDER: "essential-blocks/slider",
+  SOCIAL_SHARE: "essential-blocks/social-share",
+  SOCIAL: "essential-blocks/social",
+  TAB: "essential-blocks/tab",
+  TABLE_OF_CONTENTS: "essential-blocks/table-of-contents",
+  TEAM_MEMBER: "essential-blocks/team-member",
+  TESTIMONIAL: "essential-blocks/testimonial",
+  TOGGLE_CONTENT: "essential-blocks/toggle-content",
+  TYPING_TEXT: "essential-blocks/typing-text",
+  WOO_PRODUCT_GRID: "essential-blocks/woo-product-grid",
+  WPFORMS: "essential-blocks/wpforms",
+  WRAPPER: "essential-blocks/wrapper",
+});
+
+const EB_PRO_Blocks = Object.freeze({
+  ADVANCED_SEARCH: "essential-blocks/pro-advanced-search",
+  DATA_TABLE: "essential-blocks/pro-data-table",
+  FANCY_CHART: "essential-blocks/pro-fancy-chart",
+  // TODO: Add more pro blocks here
+});
+
+export { EB_Free_Blocks, EB_PRO_Blocks };
---
+++ tests/button.spec.js
@@ -0,0 +1,49 @@
+"use strict";
+import { test, expect } from "@wordpress/e2e-test-utils-playwright";
+import { EB_Free_Blocks } from "../helpers/block-names";
+
+test.describe("EB Buttons", () => {
+  test.beforeEach(async ({ admin }) => {
+    await admin.createNewPost({ post_type: "post", post_title: "EB Buttons" });
+  });
+
+  test.afterAll(async ({ requestUtils }) => {
+    await requestUtils.deleteAllPosts();
+  });
+
+  test("can insert a button block", async ({ editor, page }) => {
+    await editor.insertBlock({ name: EB_Free_Blocks.BUTTON });
+
+    // Select Button Type: Success
+    await page.getByLabel("Type").selectOption("success");
+    // Fill Button text
+    await page.getByLabel("Button Text").fill("Salam");
+    // Fill URL
+    await page.getByRole("textbox", { name: "URL" }).fill("https://examplexxx123.com");
+    // Toggle Open in New Tab
+    await page.getByLabel("Open in New Tab").check();
+    // Toggle Add nofollow
+    await page.getByLabel("Add nofollow").check();
+    // Alignment Left
+    await page.getByRole("button", { name: "" }).click();
+    // Width Full
+    await page.getByRole("button", { name: "Full" }).click();
+    // Expand Icon Section
+    await page.getByRole("button", { name: "Icon" }).click();
+    // Toggle Icon on
+    await page.getByLabel("Add icon").check();
+    // Icon position right
+    await page.getByRole("button", { name: "" }).nth(1).click();
+    // Icon Size 51
+    await page.locator("#inspector-range-control-0").fill("51");
+    // Icon Gap 21
+    await page.locator("#inspector-range-control-1").fill("21");
+
+    const previewPage = await editor.openPreviewPage();
+    await expect(previewPage.locator(".wp-block-essential-blocks-button")).toHaveText("Salam");
+    await expect(previewPage.getByRole("link", { name: "Salam" })).toHaveAttribute(
+      "href",
+      /https:\/\/examplexxx123.com/
+    );
+  });
+});
