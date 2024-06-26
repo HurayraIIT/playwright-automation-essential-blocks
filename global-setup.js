c391f3ab9f1de57addd1c4129290fceba651766c
Abu Hurayra <hurayraiit@gmail.com>
2024-06-26 14:55:36 +0600

    restructured the test strategy

--- global-setup.js
+++ global-setup.js
@@ -1,45 +1,20 @@
-import { test as base } from "@playwright/test";
+import { request } from "@playwright/test";
+import { RequestUtils } from "@wordpress/e2e-test-utils-playwright";
 
-// Extend the default test fixture
-export const test = base.extend({
-  // Add custom fixtures here
-  page: async ({ page }, use) => {
-    // Set up the routes
-    await page.route("**/notificationx/**", async (route) => await route.fulfill({}));
-    await page.route("**/client.crisp.chat/**", async (route) => await route.fulfill({}));
-    await page.route("**/client.relay.crisp.chat/**", async (route) => await route.fulfill({}));
-    await page.route("**/tag.marinsm.com/**", async (route) => await route.fulfill({}));
-    await page.route("**/static.hotjar.com/**", async (route) => await route.fulfill({}));
-    await page.route("**/static.doubleclick.net/**", async (route) => await route.fulfill({}));
-    await page.route("**/jnn-pa.googleapis.com/**", async (route) => await route.fulfill({}));
-    await page.route("**/googleads.g.doubleclick.net/**", async (route) => await route.fulfill({}));
-    await page.route("**/analytics.twitter.com/**", async (route) => await route.fulfill({}));
-    await page.route("**/apis.google.com/**", async (route) => await route.fulfill({}));
-    await page.route("**/play.google.com/**", async (route) => await route.fulfill({}));
-    await page.route("**/www.googletagmanager.com/**", async (route) => await route.fulfill({}));
+// https://playwright.dev/docs/test-global-setup-teardown#configure-globalsetup-and-globalteardown
+export default async function globalSetup() {
+  const requestContext = await request.newContext({
+    baseURL: process.env.WP_BASE_URL,
+  });
+  const requestUtils = new RequestUtils(requestContext, {
+    storageStatePath: process.env.WP_AUTH_STORAGE,
+    user: {
+      username: process.env.WP_USERNAME,
+      password: process.env.WP_PASSWORD,
+    },
+  });
 
-    // Add global lightbox close button handler
-    // page.on("popup", async () => {
-    //   const closeButton = page.locator(
-    //     ".dialog-lightbox-widget-content > .dialog-lightbox-close-button > .eicon-close"
-    //   );
-    //   if (await closeButton.isVisible()) {
-    //     await closeButton.click();
-    //   }
-    // });
-
-    // Wrap the goto method to include waitUntil: 'domcontentloaded'
-    const originalGoto = page.goto.bind(page);
-    page.goto = async (url, options = {}) => {
-      options.waitUntil = "domcontentloaded";
-
-      // Adding query param to bypass server cache
-      url += "?foo=bar";
-
-      return await originalGoto(url, options);
-    };
-    await use(page);
-  },
-});
-
-export { expect } from "@playwright/test";
+  // https://playwright.dev/docs/test-global-setup-teardown#example
+  await requestUtils.setupRest();
+  await requestContext.dispose();
+}
