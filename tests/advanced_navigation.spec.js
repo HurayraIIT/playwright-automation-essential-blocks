e8324e863c2c6a14f39a0885f57eaaf22431698a
Md. Ismail <111440195+mdismail-cse@users.noreply.github.com>
2024-11-22 01:14:39 +0600

    added advanced_navigation test

---
+++ tests/advanced_navigation.spec.js
@@ -0,0 +1,26 @@
+"use strict";
+import { test, expect } from "@wordpress/e2e-test-utils-playwright";
+import { EB_Free_Blocks } from "../helpers/block-names";
+import generateTimestamp from "../helpers/generator";
+
+test.describe("EB Advanced Navigation", () => {
+  test("can insert an Advanced Navigation block", async ({ admin, editor, page }) => {
+    await admin.createNewPost({ postType: "post", title: `EB Advanced Navigation ${generateTimestamp()}` });
+    await editor.insertBlock({ name: EB_Free_Blocks.ADVANCED_NAVIGATION });
+
+    //checking block visibility in editor
+    await expect(page.getByText("Click Test")).toBeVisible();
+
+    // Publish the post
+    await page.getByRole('button', { name: 'Publish', exact: true }).click();
+    await expect(page.getByRole('button', { name: 'Save', exact: true })).toBeVisible();
+    await expect(page.getByLabel('View Post')).toBeVisible();
+    const page1Promise = page.waitForEvent('popup');
+    await page.getByLabel('View Post').click();
+    const page1 = await page1Promise;
+
+    //checking block visibility in post
+    await expect(page.getByText("Click Test")).toBeVisible();
+    await expect(page1.getByText("EB Advanced Navigation 2")).toBeVisible();
+  });
+});
